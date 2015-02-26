5070e0ee6c8062314a5b8866cc4535034bb61882
Rui Baltazar <rui.p.baltazar@gmail.com>
2015-02-26 09:05:13 +0800

    WIP - time input controlled by regex. TODO: make it more user friendly

--- AddDiveViewController.swift
+++ AddDiveViewController.swift
@@ -45,15 +45,80 @@ class AddDiveViewController: UIViewController, UITextFieldDelegate, UITextViewDe
         self.view.endEditing(true)
     }
     
+    func textViewDidBeginEditing(textView: UITextView) {
+        if(textView == self.commentsTextView) {
+            if(textView.text == "Comments...") {
+                textView.text = ""
+                textView.textColor = UIColor.blackColor()
+            }
+        }
+        textView.becomeFirstResponder()
+    }
+    
+    func textViewDidEndEditing(textView: UITextView) {
+        if(textView == self.commentsTextView) {
+            if(textView.text == "") {
+                textView.text = "Comments..."
+                textView.textColor = UIColor.lightGrayColor()
+            }
+        }
+        textView.resignFirstResponder()
+
+    }
+    
     func textFieldShouldReturn(textField: UITextField!) -> Bool {
         textField.resignFirstResponder()
         return true
     }
     
-    
-    @IBAction func clearForm(sender: AnyObject) {
+    //TODO: Clean up this method
+    func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
+        if(textField == self.timeInTextView || textField == self.timeOutTextView) {
+            
+            var timeFormatExpression = "(^$)|([0-9]{1,2})|([0-9]{1,2}\\:)|([0-9]{1,2}\\:[0-9]{1,2})"
+            let currentString = textField.text as NSString
+            let newString:NSString = currentString.stringByReplacingCharactersInRange(range, withString: string)
+            let newStringLength = newString.length
+            
+            if newStringLength > 5 {
+                return false
+            }
+            
+            if newStringLength == 0 {
+                return true
+            }
+            
+            if newStringLength > 0 && newStringLength < 3 {
+                timeFormatExpression = "[0-9]{1,2}"
+            } else if newStringLength == 3 {
+                timeFormatExpression = "[0-9]{1,2}\\:"
+            } else if newStringLength > 3 {
+                timeFormatExpression = "[0-9]{1,2}\\:[0-9]{1,2}"
+            }
+            
+            //let expression = "^([0-9]{1,2})?\\:?([0-9]{1,2})?$"
+            
+            let regex = NSRegularExpression(pattern: timeFormatExpression, options: NSRegularExpressionOptions.CaseInsensitive, error: nil)
+            let numberOfMatches = regex?.numberOfMatchesInString(newString, options: nil, range: NSMakeRange(0, newString.length))
+            
+            if (numberOfMatches == 0){
+                return false
+            }
+        }
+        
+        return true
     }
     
+    @IBAction func clearForm(sender: AnyObject) {
+        self.locationTextView.text = ""
+        self.dateTextView.text = ""
+        self.timeInTextView.text = ""
+        self.timeOutTextView.text = ""
+        self.pressureInTextView.text = ""
+        self.pressureOutTextView.text = ""
+        self.commentsTextView.text = "Comments..."
+        self.commentsTextView.textColor = UIColor.lightGrayColor()
+    }
     
     @IBAction func saveDive(sender: AnyObject) {
     }
